1764f02cc0e7bb75b9aa694101d5ace61f8db9dd
Iveth <ivethanselmi@gmail.com>
2020-02-19 19:38:00 -0430

    Primer commit

---
+++ src/components/Badge.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Badge extends React.component {
+    render(){
+        return <h1>Badge!!!</h1>;
+    }
+}
+
+export default Badge;
---
+++ src/index.js
@@ -0,0 +1,15 @@
+
+//container.appendChild(element)// para añadir el elemento a el container
+
+
+//tenemos que importar herramientas:
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Badge from './components/Badge';
+
+const element = <h1>Hello</h1>;
+const container = document.getElementById('app'); 
+
+// en vez de appendchild, vamos a usar el metodo render
+// el metodo render necesita 2 parametros, 1ero: que va a renderizar, 2do: donde lo va a renderizar 
+ReactDOM.render(<Badge />,container)
