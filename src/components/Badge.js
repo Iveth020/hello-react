724ce902792c2f88b60344cd89240e08846e2979
lParedes <luisparedez@gmail.com>
2020-02-20 13:05:43 -0400

    Resuelto

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,17 +1,17 @@
 import React from 'react'
 import "./styles/Badge.css"
-import confLogo from '../images/badge-header.svg'
-import myPicture from "../images/IMG_20200103_190931_371.jpg"
 
 class Badge extends React.Component {
+  
     render(){
+       const {avatarUrl, Logo} = this.props;
         return <div className="Badge">
             <div className="Badge__header"> 
-                <img src= {confLogo} alt="Logo de la conferencia" />
+                <img src={Logo} alt="Logo de la conferencia" />
             </div>
 
             <div className="Badge__section-name">
-                <img className="Badge__avatar" src={myPicture} alt="Avatar" />
+                <img className="Badge__avatar" src={avatarUrl} alt="Avatar" />
                 <h1>{this.props.firstName} <br/>{this.props.lastName}</h1>
             </div>
 
--- src/index.js
+++ src/index.js
@@ -6,26 +6,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import "bootstrap/dist/css/bootstrap.css"
-
-<<<<<<< HEAD
-
-
 import "./global.css"
 import Badge from './components/Badge'
+import myPicture from "./images/IMG_20200103_190931_371.jpg"
+import confLogo from './images/badge-header.svg'
 
 
 const container = document.getElementById('app');
-=======
-//const element = <h1>Hello</h1>;
-const container = document.getElementById('app'); 
->>>>>>> b7737eb139c13a9442b79850fac79c92a1cdee84
 
 // en vez de appendchild, vamos a usar el metodo render
 // el metodo render necesita 2 parametros, 1ero: que va a renderizar, 2do: donde lo va a renderizar 
 ReactDOM.render(<Badge
     firstName = "Iveth"
     lastName = "Hernandez"
-    //avatarUrl = "https://images.app.goo.gl/V5tc75WtoVHRANwYA"
+    avatarUrl = {myPicture}
+    Logo={confLogo}
     ocupation = "Estudent Informatics Engineering"
     twitter = "Ivethernandez_a"
     />,container)
