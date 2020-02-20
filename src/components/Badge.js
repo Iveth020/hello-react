6e204e68d60072f680ab651d6f2aac564e727c25
Iveth <ivethanselmi@gmail.com>
2020-02-20 11:21:35 -0430

    segundo Commit

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React from 'react'
+import "./styles/Badge.css"
+import confLogo from '../images/badge-header.svg'
+import myPicture from "../images/IMG_20200103_190931_371.jpg"
 
-class Badge extends React.component {
+class Badge extends React.Component {
     render(){
-        return <h1>Badge!!!</h1>;
+        return <div className="Badge">
+            <div className="Badge__header"> 
+                <img src= {confLogo} alt="Logo de la conferencia" />
+            </div>
+
+            <div className="Badge__section-name">
+                <img className="Badge__avatar" src={myPicture} alt="Avatar" />
+                <h1>{this.props.firstName} <br/>{this.props.lastName}</h1>
+            </div>
+
+            <div className="Badge__section-info">
+              <h3>{this.props.ocupation}</h3>
+              <div>@{this.props.twitter}</div>
+            </div>
+
+            <div className="Badge__footer">
+                @platziconf
+            </div>
+
+        </div>
     }
 }
 
-export default Badge;
+export default Badge
--- src/index.js
+++ src/index.js
@@ -3,13 +3,24 @@
 
 
 //tenemos que importar herramientas:
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Badge from './components/Badge';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import "bootstrap/dist/css/bootstrap.css"
 
-const element = <h1>Hello</h1>;
-const container = document.getElementById('app'); 
+
+
+import "./global.css"
+import Badge from './components/Badge'
+
+
+const container = document.getElementById('app');
 
 // en vez de appendchild, vamos a usar el metodo render
 // el metodo render necesita 2 parametros, 1ero: que va a renderizar, 2do: donde lo va a renderizar 
-ReactDOM.render(<Badge />,container)
+ReactDOM.render(<Badge
+    firstName = "Iveth"
+    lastName = "Hernandez"
+    //avatarUrl = "https://images.app.goo.gl/V5tc75WtoVHRANwYA"
+    ocupation = "Estudent Informatics Engineering"
+    twitter = "Ivethernandez_a"
+    />,container)
